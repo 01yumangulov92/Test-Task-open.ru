387f365f08e1055e95b16722f36d63903ca67978
01yumangulov92 <01yumangulov92@gmail.com>
2020-04-24 19:44:45 +0300

    Edit xPathes

--- TestTask!/src/test/java/Pages/HomePage.java
+++ TestTask!/src/test/java/Pages/HomePage.java
@@ -7,16 +7,16 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
 
 public class HomePage {
-    String stringDollarPurchaseRate = $(By.xpath("//*[@id=\"main\"]/div/div/div[8]/section/div/div/div[1]/div/div/div/div/div[2]/table/tbody/tr[2]/td[4]/div/span")).getText();
+    String stringDollarPurchaseRate = $(By.xpath("//tbody/tr[2]/td[4]/div/span")).getText();
     String stringWithPointDollarPurchaseRate = stringDollarPurchaseRate.replace(",", ".");
     float dollarPurchaseRate = Float.parseFloat(stringWithPointDollarPurchaseRate);
-    String stringDollarBuyRate = $(By.xpath("//*[@id=\"main\"]/div/div/div[8]/section/div/div/div[1]/div/div/div/div/div[2]/table/tbody/tr[2]/td[2]/div/span")).getText();
+    String stringDollarBuyRate = $(By.xpath("//tbody/tr[2]/td[2]/div/span")).getText();
     String stringWithPointDollarBuyRate = stringDollarBuyRate.replace(",", ".");
     float dollarBuyRate = Float.parseFloat(stringWithPointDollarBuyRate);
-    String stringEuroPurchaseRate = $(By.xpath("//*[@id=\"main\"]/div/div/div[8]/section/div/div/div[1]/div/div/div/div/div[2]/table/tbody/tr[3]/td[4]/div/span")).getText();
+    String stringEuroPurchaseRate = $(By.xpath("//tbody/tr[3]/td[4]/div/span")).getText();
     String stringWithPointEuroPurchaseRate = stringEuroPurchaseRate.replace(",", ".");
     float euroPurchaseRate = Float.parseFloat(stringWithPointEuroPurchaseRate);
-    String stringEuroBuyRate = $(By.xpath("//*[@id=\"main\"]/div/div/div[8]/section/div/div/div[1]/div/div/div/div/div[2]/table/tbody/tr[2]/td[2]/div/span")).getText();
+    String stringEuroBuyRate = $(By.xpath("//tbody/tr[3]/td[2]/div/span")).getText();
     String stringWithPointEuroBuyRate = stringEuroBuyRate.replace(",", ".");
     float euroBuyRate = Float.parseFloat(stringWithPointEuroBuyRate);
 
@@ -25,7 +25,7 @@ public class HomePage {
     }
 
     public void seeOnExchangeRate() {
-        $(By.xpath("//*[@id=\"main\"]/div/div/div[8]/section/div/div/div[1]/div/div")).scrollTo();
+        $(By.xpath("//*[@class=\"main-page-exchange main-page-info__card\"]")).scrollTo();
     }
 
     public void compareExchange() {
